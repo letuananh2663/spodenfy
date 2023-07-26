862759de85827b551c295855e849ca1b8b9d7f3c
Le Tuan Anh <133185423+letuananh2663@users.noreply.github.com>
2023-07-26 22:01:55 +0700

    Update MediaItem.tsx

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -58,7 +58,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
             </div>
             <div className="flex flex-col gap-y-1 overflow-hidden">
                 <p className="text-white truncate">{data.title}</p>
-                <p className="text-neutral-400 text-sm truncate">
+                <p className="text-neutral-400 text-xs md:text-sm truncate">
                     By {data.author}
                 </p>
             </div>
